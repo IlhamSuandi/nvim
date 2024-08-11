eaf072bd84f510dab4f12b5c3ff7ea635a1db112
ilhamSuandi <ilham15suandi@gmail.com>
2024-08-11 14:50:26 +0700

    feat: remove background on gitsigns

--- lua/config/highlight.lua
+++ lua/config/highlight.lua
@@ -4,6 +4,10 @@ M.setup = function()
   vim.api.nvim_set_hl(0, "CursorLine", { bg = "#424757" })
   vim.api.nvim_set_hl(0, "LineNr", { fg = "#8b9199" })
   vim.api.nvim_set_hl(0, "Visual", { bg = "#424757" })
+  vim.api.nvim_set_hl(0, "GitSignsAdd", { bg = "none" })
+  vim.api.nvim_set_hl(0, "GitSignsChange", { bg = "none" })
+  vim.api.nvim_set_hl(0, "GitSignsDelete", { bg = "none" })
+
   -- vim.api.nvim_set_hl(0, "Pmenu", { bg = "#424757" })
   -- vim.api.nvim_set_hl(0, "PmenuSel", { bg = "white" })
 end
