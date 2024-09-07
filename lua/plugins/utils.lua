ab333feb99ed8a0304ddbde0cb640805c53518f0
ilhamSuandi <ilham15suandi@gmail.com>
2024-09-07 16:45:11 +0700

    feat(rm): remove unused comments

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -334,7 +334,6 @@ return {
   {
     "frankroeder/parrot.nvim",
     dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
-    -- optionally include "rcarriga/nvim-notify" for beautiful notifications
     config = function()
       require("config.parrot")
     end,
