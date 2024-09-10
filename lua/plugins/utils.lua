5f27e176687f48c0a4a8194a59a1edb3a006654f
ilhamSuandi <ilham15suandi@gmail.com>
2024-09-11 00:17:42 +0700

    feat: add tailwind tool plugin
    to fold tailwind, etc..

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -398,4 +398,17 @@ return {
       }
     end,
   },
+
+  -- NOTE : tailwind tool
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {}, -- your configuration
+  },
 }
