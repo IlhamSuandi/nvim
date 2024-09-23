a9188531633d217a408237fe79d8dc43b5a095d2
ilhamSuandi <ilham15suandi@gmail.com>
2024-09-23 16:34:03 +0700

    fix: fix highlight after changing colorscheme

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    require("config.highlight").setup()
+  end,
+})
