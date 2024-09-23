e9e9429916d5ec360e46f3d1951bdbe80c1e971f
ilhamSuandi <ilham15suandi@gmail.com>
2024-09-23 17:23:17 +0700

    feat: automatically remove lualine bg

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,4 +1,6 @@
 local icons = LazyVim.config.icons
+local auto_theme_custom = require("lualine.themes.auto")
+auto_theme_custom.normal.c.bg = "none"
 
 return {
   sections = {
@@ -52,6 +54,7 @@ return {
     },
   },
   options = {
+    theme = auto_theme_custom,
     section_separators = { left = "", right = "" },
   },
 }
