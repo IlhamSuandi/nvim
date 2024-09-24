26c3057f521eb251c79a57c2833700e2b6b70a62
ilhamSuandi <ilham15suandi@gmail.com>
2024-09-24 13:01:15 +0700

    style: change lualine recording style

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,29 +1,36 @@
 local icons = LazyVim.config.icons
 local auto_theme_custom = require("lualine.themes.auto")
 auto_theme_custom.normal.c.bg = "none"
+auto_theme_custom.normal.b.bg = "none"
 
 return {
   sections = {
     lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
     lualine_x = {
-            -- stylua: ignore
-            {
-              function() return require("noice").api.status.mode.get() end,
-              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-              color = function() return LazyVim.ui.fg("Constant") end,
-            },
-            -- stylua: ignore
-            {
-              function() return "  " .. require("dap").status() end,
-              cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-              color = function() return LazyVim.ui.fg("Debug") end,
-            },
-            -- stylua: ignore
-            {
-              require("lazy.status").updates,
-              cond = require("lazy.status").has_updates,
-              color = function() return LazyVim.ui.fg("Special") end,
-            },
+      -- stylua: ignore
+      {
+        function()
+          local noiceStatus = require("noice").api.status.mode.get() 
+          if noiceStatus:find "recording" then
+            return noiceStatus
+          end
+          return ""
+        end,
+        cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+        color = function() return LazyVim.ui.fg("Constant") end,
+      },
+      -- stylua: ignore
+      {
+        function() return "  " .. require("dap").status() end,
+        cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+        color = function() return LazyVim.ui.fg("Debug") end,
+      },
+      -- stylua: ignore
+      {
+        require("lazy.status").updates,
+        cond = require("lazy.status").has_updates,
+        color = function() return LazyVim.ui.fg("Special") end,
+      },
       {
         "diff",
         symbols = {
