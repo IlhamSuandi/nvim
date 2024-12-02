e6a168a08803ece37827518f9280f775562208ed
ilhamSuandi <ilham15suandi@gmail.com>
2024-12-03 05:35:09 +0700

    refactor: move markdown opts to separate file

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -67,57 +67,10 @@ return {
   {
     "MeanderingProgrammer/markdown.nvim",
     main = "render-markdown",
-    opts = {
-      checkbox = {
-        checked = {
-          icon = "[✔] ",
-          highlight = "RenderMarkdownChecked",
-        },
-        unchecked = {
-          icon = "󰄱 ",
-          highlight = "RenderMarkdownUnchecked",
-        },
-        custom = {
-          unchecked = { raw = "[ ]", rendered = "󰄱 ", highlight = "RenderMarkdownUnchecked" },
-          checked = { raw = "[x]", rendered = "[✔] ", highlight = "RenderMarkdownChecked" },
-          capitalized_checked = { raw = "[X]", rendered = "[✔] ", highlight = "RenderMarkdownChecked" },
-          right_arrow = { raw = "[>]", rendered = "[󰥔] ", highlight = "DiagnosticWarn" },
-          tilde = { raw = "[~]", rendered = "[✘] ", highlight = "RenderMarkdownTilde" },
-          important = { raw = "[!]", rendered = "[] ", highlight = "DiagnosticError" },
-        },
-      },
-      callout = {
-        note = { raw = "[!NOTE]", rendered = "󰋽 Note", highlight = "RenderMarkdownInfo" },
-        tip = { raw = "[!TIP]", rendered = "󰌶 Tip", highlight = "RenderMarkdownSuccess" },
-        important = { raw = "[!IMPORTANT]", rendered = "󰅾 Important", highlight = "RenderMarkdownError" },
-        warning = { raw = "[!WARNING]", rendered = "󰀪 Warning", highlight = "RenderMarkdownWarn" },
-        caution = { raw = "[!CAUTION]", rendered = "󰳦 Caution", highlight = "RenderMarkdownError" },
-        abstract = { raw = "[!ABSTRACT]", rendered = "󰨸 Abstract", highlight = "RenderMarkdownInfo" },
-        summary = { raw = "[!SUMMARY]", rendered = "󰨸 Summary", highlight = "RenderMarkdownInfo" },
-        tldr = { raw = "[!TLDR]", rendered = "󰨸 Tldr", highlight = "RenderMarkdownInfo" },
-        info = { raw = "[!INFO]", rendered = "󰋽 Info", highlight = "RenderMarkdownInfo" },
-        todo = { raw = "[!TODO]", rendered = "󰗡 Todo", highlight = "RenderMarkdownInfo" },
-        hint = { raw = "[!HINT]", rendered = "󰌶 Hint", highlight = "RenderMarkdownSuccess" },
-        success = { raw = "[!SUCCESS]", rendered = "󰄬 Success", highlight = "RenderMarkdownSuccess" },
-        check = { raw = "[!CHECK]", rendered = "󰄬 Check", highlight = "RenderMarkdownSuccess" },
-        done = { raw = "[!DONE]", rendered = "󰄬 Done", highlight = "RenderMarkdownSuccess" },
-        question = { raw = "[!QUESTION]", rendered = "󰘥 Question", highlight = "RenderMarkdownWarn" },
-        help = { raw = "[!HELP]", rendered = "󰘥 Help", highlight = "RenderMarkdownWarn" },
-        faq = { raw = "[!FAQ]", rendered = "󰘥 Faq", highlight = "RenderMarkdownWarn" },
-        attention = { raw = "[!ATTENTION]", rendered = "󰀪 Attention", highlight = "RenderMarkdownWarn" },
-        failure = { raw = "[!FAILURE]", rendered = "󰅖 Failure", highlight = "RenderMarkdownError" },
-        fail = { raw = "[!FAIL]", rendered = "󰅖 Fail", highlight = "RenderMarkdownError" },
-        missing = { raw = "[!MISSING]", rendered = "󰅖 Missing", highlight = "RenderMarkdownError" },
-        danger = { raw = "[!DANGER]", rendered = "󱐌 Danger", highlight = "RenderMarkdownError" },
-        error = { raw = "[!ERROR]", rendered = "󱐌 Error", highlight = "RenderMarkdownError" },
-        bug = { raw = "[!BUG]", rendered = "󰨰 Bug", highlight = "RenderMarkdownError" },
-        example = { raw = "[!EXAMPLE]", rendered = "󰉹 Example", highlight = "RenderMarkdownHint" },
-        quote = { raw = "[!QUOTE]", rendered = "󱆨 Quote", highlight = "RenderMarkdownQuote" },
-        cite = { raw = "[!CITE]", rendered = "󱆨 Cite", highlight = "RenderMarkdownQuote" },
-        recurrent = { raw = "[!RECURRENT]", rendered = "󰨸 Recurrent", highlight = "RenderMarkdownWarn" },
-      },
-    },
     name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    opts = {
+      require("config.markdown-preview"),
+    },
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
   },
 
@@ -489,5 +442,8 @@ return {
     "andymass/vim-matchup",
     lazy = false,
     event = "VeryLazy",
+    config = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
   },
 }
