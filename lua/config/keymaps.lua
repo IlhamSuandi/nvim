2508a21a27b5b6fbe08fb203816c1fd2a427c467
ilhamSuandi <ilham15suandi@gmail.com>
2024-12-17 22:25:02 +0700

    perf: remove unused keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -39,33 +39,22 @@ map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
 
 map("i", "<C-l>", "<Left>", { desc = "go to left in insert mode" })
 map("i", "<C-h>", "<Left>", { desc = "go to left in insert mode" })
--- --
-
--- NOTE : noice mappings
-map("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", { desc = "noice dismiss notification" })
-map("n", "<leader>nl", "<cmd>NoiceLast<CR>", { desc = "noice last notification" })
-map("n", "<leader>nt", "<cmd>NoiceTelescope<CR>", { desc = "noice telescope" })
 
 -- NOTE : lspsaga
 map({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<cr>", opts("lspsaga code action"))
-map("n", "<leader>cd", "<cmd>Lspsaga peek_definition<cr>", opts("lspsaga peek definition"))
-map("n", "<leader>co", "<cmd>Lspsaga outline<cr>", opts("lspsaga outline"))
+map("n", "<leader>cp", "<cmd>Lspsaga peek_definition<cr>", opts("lspsaga peek definition"))
+map("n", "<leader>cs", "<cmd>Lspsaga outline<cr>", opts("lspsaga outline"))
 map("n", "<leader>ce", "<cmd>Lspsaga show_buf_diagnostics<cr>", opts("lspsaga show buf diagnostics"))
 map("n", "<leader>cE", "<cmd>Lspsaga show_workspace_diagnostics<cr>", opts("lspsaga show workspace diagnostics"))
 map("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts("lspsaga hover doc"))
-map("n", "en", "<cmd>Lspsaga diagnostic_jump_next<cr>", opts("lspsaga jump to next diagnostic"))
-map("n", "ep", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opts("lspsaga jump to prev diagnostic"))
-map("n", "es", "<cmd>Lspsaga show_line_diagnostics<cr>", opts("lspsaga show line diagnostics"))
+map("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>", opts("lspsaga jump to next diagnostic"))
+map("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opts("lspsaga jump to prev diagnostic"))
+map("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<cr>", opts("lspsaga show line diagnostics"))
+-- map("n", "<leader>uW", "<cmd>Lspsaga winbar_toggle<cr>", opts("lspsaga winbar toggle"))
 
 -- NOTE : remove keymap
 unmap("n", "<leader>uC")
 map("n", "<leader>uC", "<cmd>Themery<cr>", opts("Themery Change Theme"))
-
--- NOTE : true-zen keymap
-map("n", "<leader>za", "<cmd>TZAtaraxis<cr>", opts("Toggle Ataraxis Mode"))
-map("n", "<leader>zm", "<cmd>TZMinimalist<cr>", opts("Toggle Minimalist Mode"))
-map("n", "<leader>zn", "<cmd>TZNarrow<cr>", opts("Toggle Narrow Mode"))
-map("n", "<leader>zf", "<cmd>TZFocus<cr>", opts("Toggle Focus Mode"))
 
 -- NOTE : obsidian keymap
 map("n", "<leader>Of", "<cmd>ObsidianQuickSwitch<cr>", opts("Obsidian Quick Switch"))
@@ -82,3 +71,11 @@ map("n", "<leader>gxb", "<Plug>(git-conflict-both)", opts("accept both"))
 map("n", "<leader>gx0", "<Plug>(git-conflict-none)", opts("accept none"))
 map("n", "<leader>gxp", "<Plug>(git-conflict-prev-conflict)", opts("conflict prev"))
 map("n", "<leader>gxn", "<Plug>(git-conflict-next-conflict)", opts("conflict next"))
+
+-- NOTE: remap buffer goto buffer
+for i = 1, 9 do
+  map("n", "<leader>" .. i, string.format("<cmd>BufferLineGoToBuffer %d<cr>", i), opts("Go to buffer " .. i))
+end
+
+-- NOTE: add find word mapping
+map("n", "<leader>fw", "<cmd>FzfLua live_grep<cr>", opts("find word"))
