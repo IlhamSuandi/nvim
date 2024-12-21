4d721fb305546f0c1fc421ce9dfe0e25384c9784
ilhamSuandi <ilham15suandi@gmail.com>
2024-12-22 02:13:32 +0700

    chore: rm recommended

--- lua/plugins/lsp/dap.lua
+++ lua/plugins/lsp/dap.lua
@@ -1,9 +1,7 @@
 return {
   {
     "mfussenegger/nvim-dap",
-    recommended = true,
     desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
-
     dependencies = {
       "rcarriga/nvim-dap-ui",
       -- virtual text for the debugger
