90578010fee46afe94c82b66c00d48c95ea96bc7
ilhamSuandi <ilham15suandi@gmail.com>
2024-12-22 02:51:53 +0700

    feat: add plugin to jump between text

---
+++ lua/plugins/utils/flash.lua
@@ -0,0 +1,33 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    highlight = {
+      -- show a backdrop with hl FlashBackdrop
+      backdrop = false,
+      -- Highlight the search matches
+      matches = true,
+      -- extmark priority
+      priority = 5000,
+      groups = {
+        match = "FlashMatch",
+        current = "FlashCurrent",
+        backdrop = "FlashBackdrop",
+        label = "FlashLabel",
+      },
+    },
+    modes = {
+      char = {
+        enabled = true,
+        highlight = { backdrop = false },
+      },
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+  },
+}
