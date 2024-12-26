78020653f06635301e86c32070bc67231ac3580f
ilhamSuandi <ilham15suandi@gmail.com>
2024-12-26 22:19:33 +0700

    feat: disable image nvim

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,12 @@
+local function disable(plugins)
+  local disabled_plugins = {}
+  for _, plugin in ipairs(plugins) do
+    table.insert(disabled_plugins, { plugin, enabled = false })
+  end
+
+  return disabled_plugins
+end
+
+return disable {
+  "3rd/image.nvim",
+}
