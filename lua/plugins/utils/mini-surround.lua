887072784a0939d1be119e5729c61a939f6144b2
ilhamSuandi <ilham15suandi@gmail.com>
2024-12-27 02:05:18 +0700

    feat: add mini surround plugins

---
+++ lua/plugins/utils/mini-surround.lua
@@ -0,0 +1,21 @@
+return {
+  "echasnovski/mini.surround",
+  event = "VeryLazy",
+  lazy = false,
+  config = function()
+    require("mini.surround").setup {
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+
+        suffix_last = "l", -- Suffix to search with "prev" method
+        suffix_next = "n", -- Suffix to search with "next" method
+      },
+    }
+  end,
+}
