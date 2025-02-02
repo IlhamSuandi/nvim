e1a0bdd2150214a6cc7c1ca3ad39b2e8800d2f4c
ilhamSuandi <ilham15suandi@gmail.com>
2025-02-02 15:04:30 +0700

    build(python): add python default path

--- lua/options.lua
+++ lua/options.lua
@@ -8,3 +8,6 @@ vim.diagnostic.config { virtual_text = false }
 vim.notify = Snacks.notifier
 
 vim.opt.wrap = false -- Disable line wrap
+
+vim.g.python3_host_prog = "/usr/bin/python3"
+vim.g.loaded_python3_provider = 1
