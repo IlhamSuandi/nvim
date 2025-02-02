bdbd210c575deb693def027645a57cca69ad215a
ilhamSuandi <ilham15suandi@gmail.com>
2025-02-02 15:05:07 +0700

    feat(dadbod): connect to default db

--- lua/plugins/utils/dadbod.lua
+++ lua/plugins/utils/dadbod.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     { "tpope/vim-dadbod", lazy = true },
     { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+    { "tpope/vim-dotenv", lazy = false },
   },
   cmd = {
     "DBUI",
@@ -14,6 +15,8 @@ return {
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_env_variable_url = "DB_URL"
+    vim.g.db_ui_dotenv_variable_prefix = "DB_UI_"
   end,
   keys = {
     { "<leader>D", "<cmd>DBUIToggle<cr>", desc = "Open Database UI" },
